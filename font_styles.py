dc5be3a2aa719f8c0e261e0d69a99dc4e5694048
Hayden Caldwell <hade_caldwell@msn.com>
2024-01-21 04:55:51 +0000

    Add ASCII Art to ascii_art.py for GAME_TITLE and RULES

---
+++ font_styles.py
@@ -0,0 +1,15 @@
+class styles:
+    """
+    Font styles used throughout the application.
+    """
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    RED = '\033[31m'
+    BLUE = '\033[34m'
+    BLUE_BOLD = '\033[1;34m'
+    BOLD = '\033[1m'
+    FIN = '\033[0m'
+    GRN_Y = "\033[32mY\033[0m"
+    GRN_YES = "\033[32mYES\033[0m"
+    RED_N = "\033[31mN\033[0m"
+    RED_NO = "\033[31mNO\033[0m"
